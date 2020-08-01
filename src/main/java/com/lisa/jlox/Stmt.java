3a2a22ed533c4d71bec01c73ba39271e778349aa
lisa-belle-suzuki <lisamarines_bb4@icloud.com>
2020-08-01 22:26:45 +0900

    GenerateAstにLogicalを足した

--- src/main/java/com/lisa/jlox/Stmt.java
+++ src/main/java/com/lisa/jlox/Stmt.java
@@ -6,11 +6,12 @@ abstract class Stmt {
   interface Visitor<R> {
     R visitBlockStmt(Block stmt);
     R visitExpressionStmt(Expression stmt);
+    R visitIfStmt(If stmt);
     R visitPrintStmt(Print stmt);
     R visitVarStmt(Var stmt);
   }
   static class Block extends Stmt {
-    Block(List<Stmt> statements) {
+    Block(List<stmt> statements) {
       this.statements = statements;
     }
 
@@ -19,7 +20,7 @@ abstract class Stmt {
       return visitor.visitBlockStmt(this);
     }
 
-    final List<Stmt> statements;
+    final List<stmt> statements;
   }
   static class Expression extends Stmt {
     Expression(Expr expression) {
@@ -32,6 +33,22 @@ abstract class Stmt {
     }
 
     final Expr expression;
+  }
+  static class If extends Stmt {
+    If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+      this.condition = condition;
+      this.thenBranch = thenBranch;
+      this.elseBranch = elseBranch;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitIfStmt(this);
+    }
+
+    final Expr condition;
+    final Stmt thenBranch;
+    final Stmt elseBranch;
   }
   static class Print extends Stmt {
     Print(Expr expression) {
