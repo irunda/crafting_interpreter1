0b978a48f7b9d86a353c488fa9d4e250c7407fdc
lisa-belle-suzuki <lisamarines_bb4@icloud.com>
2020-08-16 02:27:00 +0900

    12.1まで終わった！

--- src/main/java/com/lisa/jlox/Interpreter.java
+++ src/main/java/com/lisa/jlox/Interpreter.java
@@ -166,6 +166,15 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
 
+    @override
+    public Void visitClassStmt(Stmt.Class stmt) {
+	environment.define(stmt.name.lexeme, null);
+	LoxClass klass = new LoxClass(stmt.name.lexeme);
+	environment.assign(stmt.name, klass);
+	return null;
+    }
+    
+
     void executeBlock(List<Stmt> statements, Environment environment){
         Environment previous = this.environment;
         try {
